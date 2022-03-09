895d61b936841da41212c6ac9b38bd51fcbd25fc
Хорьков Вячеслав Борисович <Khorkov.V.Bo@sberbank.ru>
2022-03-09 20:30:45 +0300

    fix file contents

--- main/common/src/main/java/ru/sbrf/server/common/messages/RequestTypes.java
+++
@@ -1,8 +0,0 @@
-package ru.sbrf.server.common.messages;
-
-public enum RequestTypes {
-    XML,
-    JSON,
-    REF,
-    XLSX
-}
--- main/server/src/main/java/ru/sbrf/server/repository/ClientCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.sbrf.server.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.sbrf.server.entity.Client;
-
-@Repository
-public interface ClientCrudRepository extends CrudRepository<Client, Long> {
-}
